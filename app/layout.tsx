6b78ff37d12a415d52fef6f4ca9bf4edec6ee056
Bilal <bilalnadeem201c@gmail.com>
2026-05-12 09:09:22 +0500

    Added Open Graph tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,31 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Eank Your Business with Easy Where Solution",
-  description: "we are Easy Where Solution we provide complete digital exsistance to your business",
-};
+  title: "Easy Where Solution | AI & Digital Business Services",
+  
+  description:
+    "Easy Where Solution provides AI tools, digital marketing, and complete online business solutions.",
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body>{children}</body>
-    </html>
-  );
-}
+  openGraph: {
+    title: "Easy Where Solution",
+    
+    description:
+      "We provide AI tools and digital business solutions.",
+
+    url: "https://my-website-phi-lake-53.vercel.app",
+
+    siteName: "Easy Where Solution",
+
+    images: [
+      {
+        url: "logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Easy Where Solution",
+      },
+    ],
+
+    locale: "en_US",
+    type: "website",
+  },
+};
