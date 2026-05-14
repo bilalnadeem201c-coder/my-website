c7f6d0b950ce8432c79f3d378a5e6ebc893521df
Bilal <bilalnadeem201c@gmail.com>
2026-05-14 07:47:03 +0500

    Changed meta title and description

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
-import { useState, useEffect } from "react"
+
+import { useState, useEffect, useRef } from "react"
+import gsap from "gsap"
 import Marquee from "react-fast-marquee"
 import "./globals.css"
 
